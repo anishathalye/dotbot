56d8d05e777e3831b9a0fab0bbdd45af72185b9f
Joshua Blum <joshblum@mit.edu>
2015-04-24 12:47:42 +0300

    Provide exception information when reading config

---
+++ dotbot/util/string.py
@@ -0,0 +1,4 @@
+def indent_lines(string, amount=2, delimiter='\n'):
+    whitespace = ' ' * amount
+    sep = '%s%s' % (delimiter, whitespace)
+    return '%s%s' % (whitespace, sep.join(string.split(delimiter)))
