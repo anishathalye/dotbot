a3bc61c97d3092828b065c07a36aef2b219fda8d
Brian Knobbs <brian@redlattice.com>
2015-06-12 10:10:47 -0400

    Trying out ordered YAML

--- dotbot/config.py
+++ dotbot/config.py
@@ -1,5 +1,24 @@
 import yaml
 from .util import string
+from collections import OrderedDict
+
+
+def ordered_load(stream, Loader=yaml.Loader, object_pairs_hook=OrderedDict):
+    """ Stolen from SO - http://stackoverflow.com/questions/5121931/in-python-how-can-you-load-yaml-mappings-as-ordereddicts
+
+    Loads a YAML file using an OrderedDict so the ordering of the values are
+    maintained
+    """
+    class OrderedLoader(Loader):
+        pass
+    def construct_mapping(loader, node):
+        loader.flatten_mapping(node)
+        return object_pairs_hook(loader.construct_pairs(node))
+    OrderedLoader.add_constructor(
+        yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+        construct_mapping)
+    return yaml.load(stream, OrderedLoader)
+
 
 class ConfigReader(object):
     def __init__(self, config_file_path):
@@ -8,7 +27,7 @@ class ConfigReader(object):
     def _read(self, config_file_path):
         try:
             with open(config_file_path) as fin:
-                data = yaml.load(fin)
+                data = ordered_load(fin)
             return data
         except Exception as e:
             msg = string.indent_lines(str(e))
