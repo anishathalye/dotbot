8d08e4b1adfd00092835b6aed0edecd222f49977
Anish Athalye <me@anishathalye.com>
2018-04-06 22:09:57 +0530

    Add tests for globbing

---
+++ test/tests/link-glob-multi-star.bash
@@ -0,0 +1,31 @@
+test_description='link glob'
+. '../test-lib.bash'
+
+test_expect_success 'setup' '
+mkdir ${DOTFILES}/config &&
+mkdir ${DOTFILES}/config/foo &&
+mkdir ${DOTFILES}/config/bar &&
+echo "apple" > ${DOTFILES}/config/foo/a &&
+echo "banana" > ${DOTFILES}/config/bar/b &&
+echo "cherry" > ${DOTFILES}/config/bar/c
+'
+
+test_expect_success 'run' '
+run_dotbot -v <<EOF
+- defaults:
+    link:
+      glob: true
+      create: true
+- link:
+    ~/.config/: config/*/*
+EOF
+'
+
+test_expect_success 'test' '
+! readlink ~/.config/ &&
+! readlink ~/.config/foo &&
+readlink ~/.config/foo/a &&
+grep "apple" ~/.config/foo/a &&
+grep "banana" ~/.config/bar/b &&
+grep "cherry" ~/.config/bar/c
+'
