b0ce63904c98a0f3f6547dc0d770aad22b08a8ef
Anish Athalye <me@anishathalye.com>
2018-10-16 20:22:50 -0400

    Add test for conditional linking

--- test/tests/link-if.bash
+++ test/tests/link-if.bash
@@ -1,2 +1,32 @@
 test_description='link if'
 . '../test-lib.bash'
+
+test_expect_success 'setup' '
+mkdir ~/d
+echo "apple" > ${DOTFILES}/f
+'
+
+test_expect_success 'run' '
+run_dotbot <<EOF
+- link:
+    ~/.f:
+        path: f
+        if: "true"
+    ~/.g:
+        path: f
+        if: "false"
+    ~/.h:
+        path: f
+        if: "[[ -d ~/d ]]"
+    ~/.i:
+        path: f
+        if: "badcommand"
+EOF
+'
+
+test_expect_success 'test' '
+grep "apple" ~/.f &&
+! test -f ~/.g &&
+grep "apple" ~/.h &&
+! test -f ~/.i
+'
