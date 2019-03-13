2ecb3ce35dd6bf52b8bcf3f2e72b206f0e2f37b3
Benjamin Motz <benjamin.motz@mailbox.org>
2019-03-13 10:53:50 +0100

    Provide more accurate warning message

--- dotbot/plugins/link.py
+++ dotbot/plugins/link.py
@@ -255,10 +255,10 @@ class Link(dotbot.Plugin):
         # again, we use absolute_source to check for existence
         elif not self._exists(absolute_source):
             if self._is_link(link_name):
-                self._log.warning('Nonexistent target %s -> %s' %
+                self._log.warning('Nonexistent source %s -> %s' %
                     (link_name, source))
             else:
-                self._log.warning('Nonexistent target for %s : %s' %
+                self._log.warning('Nonexistent source for %s : %s' %
                     (link_name, source))
         else:
             self._log.lowinfo('Link exists %s -> %s' % (link_name, source))
